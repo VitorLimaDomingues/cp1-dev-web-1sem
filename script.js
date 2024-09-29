325499936676dd31c907d6569b17273d83b40f87
Vitor de Lima Domingues <vitorlimadomingues@gmail.com>
2024-09-29 18:27:21 -0300

    Exemplificando a declaração de variáveis: undefined, null e com valor

---
+++ script.js
@@ -0,0 +1,10 @@
+// Exemplificando como declarar uma variável do tipo undefined, vazio e com valor.
+
+let valor // declarando a variável como o valor "undefined"
+console.log(valor)
+
+let valor2 = null // Declarando a variável com o valor "vazio/null"
+console.log(valor2)
+
+let valor3 = 'fiap'
+console.log(valor3) // Declarando a variável com valor.
