121ec5756597b453655801490e22a3b52123c6ea
Vitor de Lima Domingues <vitorlimadomingues@gmail.com>
2024-09-30 21:00:57 -0300

    Declarando a variável como Int, convertendo para Float, e apresentando o tipo dela - Giovanni Romano Provazi

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Exemplificando como declarar uma variável do tipo undefined, vazio e com valor.
+// 1 ) Exemplificando como declarar uma variável do tipo undefined, vazio e com valor.
 
 let valor // declarando a variável como o valor "undefined"
 console.log(valor)
@@ -10,12 +10,22 @@ let valor3 = 'fiap'
 console.log(valor3) // Declarando a variável com valor.
 
 
-// Declarando a variável como String, depois convertendo para float, e apresentando o tipo da variável.
+//  2 ) Declarando a variável como String, depois convertendo para float, e apresentando o tipo da variável.
 
 let variavel = '123'
 console.log(typeof variavel) // Declarando a variável como "String".
 
 let variavel2 = parseFloat(variavel)
  console.log(variavel2) // Convertendo a variável "String" para "Float".
-
+ 
  console.log(typeof variavel2) // Apresentando o tipo da variável depois da conversão.
+
+// 3 ) Declarando a variável como Int, depois convertendo para float, e apresentando o tipo da variável.
+
+let numeroInteiro = 456
+console.log(numeroInteiro)
+console.log(typeof numeroInteiro) // Declarando a variável como "Int"
+
+let numeroFloat = numeroInteiro * 1.0 // Convertendo a variável "Int" para "Float"
+console.log(numeroFloat) 
+console.log(typeof numeroFloat) // Apresentando o tipo da variável.
